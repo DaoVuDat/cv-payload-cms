4ff92105b443be6c02d8b33ab252afcd01012b32
Dat Dao Vu <dat.daovu@gmail.com>
2025-06-01 16:26:07 +0700

    add css scaffold with tailwind v4

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <cover-l centered="center-l" space="var(--gutter)">
+        <center-l>
+          <h1 class="text-red-500">Center testing</h1>
+        </center-l>
+    </cover-l>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,8 +1,27 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
+  devtools: {enabled: true},
+  future: {
+    compatibilityVersion: 4,
+  },
   devServer: {
     port: 3001,
-  }
+  },
+  vue: {
+    compilerOptions: {
+      // isCustomElement: (tag) => tag.includes('-l') || tag === 'stack-l'
+      isCustomElement: (tag) => tag.includes('-l')
+    }
+  },
+  css: [
+    '~/assets/css/global.css',
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
