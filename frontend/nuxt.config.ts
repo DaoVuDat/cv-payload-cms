cc821c3d27591343fa7d96d917a7b55a80b19a5a
Dat Dao Vu <dat.daovu@gmail.com>
2025-06-05 15:36:04 +0700

    adding scramble text gsap for hero section

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,18 +4,24 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: {enabled: true},
+
   future: {
     compatibilityVersion: 4,
   },
+
   devServer: {
     port: 3001,
   },
+
   css: [
     '~/assets/css/global.css',
   ],
+
   vite: {
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  modules: ['@nuxt/icon']
 })
